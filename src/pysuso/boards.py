8222e5b6470233f1b47d8138067e5d9f36329503
Robert Schmidt <allthingscoding21th@gmail.com>
2024-02-26 06:38:28 +0100

    Implement Coordinate

--- src/pysuso/boards.py
+++ src/pysuso/boards.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Coordinate:
-    pass
+    row: int
+    col: int
+
+    def __post_init__(self) -> None:
+        if self.row < 0 or self.row >= 9:
+            raise ValueError(f"Row index needs to be between zero and eight. Given {self.row}")
+        if self.col < 0 or self.col >= 9:
+            raise ValueError(f"Column index needs to be between zero and eight. Given {self.col}")
 
 
 class Board:
