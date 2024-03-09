0a96f892577e4afa5e6483973913f5bd738db9e6
Robert Schmidt <allthingscoding21th@gmail.com>
2024-03-09 13:07:53 +0100

    Add exceptions doc strings

--- src/pysuso/exceptions.py
+++ src/pysuso/exceptions.py
@@ -1,14 +1,17 @@
-class InvalidCellValueException(ValueError):
-    pass
-
-
-class BoardNotSolvableException(BaseException):
-    pass
-
-
-class InvalidIndexException(ValueError):
-    pass
+"""This module contains expections specific to PySuSo"""
 
 
 class InvalidBoardException(ValueError):
-    pass
+    """Raise in case the board cannot be created from the provided values"""
+
+
+class InvalidCellValueException(ValueError):
+    """Raise in case a value is not allowed in a cell"""
+
+
+class InvalidIndexException(ValueError):
+    "Raise in case an index is not valid"
+
+
+class BoardNotSolvableException(BaseException):
+    """Raise in case the board does not have a solution."""
